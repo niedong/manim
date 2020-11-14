567efa28e9fcfbc17d73472d842a87083fd5fda0
niedong <niedong0816@126.com>
2020-11-14 11:14:51 +0800

    Modification in Git.py

--- Git.py
+++ Git.py
@@ -3,5 +3,5 @@ from manimlib.imports import *
 
 class GitScene(Scene):
     def construct(self):
-        self.play(ShowCreation(SVGMobject("assets/git/GitHub-Mark.svg").scale(8).shift(DOWN * 5)), run_time=2)
+        self.play(ShowCreation(SVGMobject("assets/git/git.svg")), run_time=2)
         self.wait(2)
